b5732f54cb3d4b53f4a64e984c8e567c61aa365e
aldus <drp@cms-lab.com>
2017-01-14 16:37:27 +0100

    initial commit.

---
+++ CHANGELOG.md
@@ -0,0 +1,9 @@
+## lib_datetools
+
+A simple (experimental) class for transform/modify dates in differ formats.
+
+#### changelog
+Please keep in mimd that this log may be inaccurate!
+
+#### 0.1.0.0
+- first private setup and run for LEPTON-CMS
---
+++ info.php
@@ -0,0 +1,49 @@
+<?php
+
+/**
+ * This file is part of an ADDON for use with LEPTON Core.
+ * This ADDON is released under the GNU GPL.
+ * Additional license terms can be seen in the info.php of this module.
+ *
+ *
+ * @author          LEPTON Project
+ * @copyright       2010-2017 LEPTON Project
+ * @link            https://www.LEPTON-cms.org
+ * @license         http://www.gnu.org/licenses/gpl.html
+ * @license_terms   please see info.php of this module
+ *
+ */
+
+// include class.secure.php to protect this file and the whole CMS!
+if (defined('LEPTON_PATH')) {	
+	include(LEPTON_PATH.'/framework/class.secure.php'); 
+} else {
+	$oneback = "../";
+	$root = $oneback;
+	$level = 1;
+	while (($level < 10) && (!file_exists($root.'/framework/class.secure.php'))) {
+		$root .= $oneback;
+		$level += 1;
+	}
+	if (file_exists($root.'/framework/class.secure.php')) { 
+		include($root.'/framework/class.secure.php'); 
+	} else {
+		trigger_error(sprintf("[ <b>%s</b> ] Can't include class.secure.php!", $_SERVER['SCRIPT_NAME']), E_USER_ERROR);
+	}
+}
+// end include class.secure.php
+
+$module_directory	= 'lib_datetools';
+$module_name		= 'lib DateTools';
+$module_function	= 'library';
+$module_version		= '0.1.0.0';
+$module_platform	= '2.4';
+$module_delete		=  true;
+$module_author		= 'LEPTON Project';
+$module_license		= 'GNU General Public License';
+$module_license_terms   = '-';
+$module_description	= 'Experimental class for transforming and formatting dates.';
+$module_guid		= 'B3A548D6-4ACB-4DD8-B377-4E9B0C750599';
+$module_home		= ' https://www.lepton-cms.org';
+
+?>
